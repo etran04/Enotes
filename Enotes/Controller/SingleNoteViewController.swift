e6ccf79e79b2bfa185f80f9844b0e8f7e896e991
Eric Tran <erictritran@yahoo.com>
2018-06-13 00:50:30 -0700

    Initial Commit

---
+++ Enotes/Controller/SingleNoteViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SingleNoteViewController.swift
+//  Enotes
+//
+//  Created by Eric Tran on 6/13/18.
+//  Copyright © 2018 Eric Tran. All rights reserved.
+//
+
+import UIKit
+
+class SingleNoteViewController: UIViewController {
+
+}
