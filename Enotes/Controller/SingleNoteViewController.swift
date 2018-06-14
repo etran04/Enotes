ef3895af117648430b3bd300b35b4536e0169e93
Eric Tran <erictritran@yahoo.com>
2018-06-14 01:03:59 -0700

    Perform segue from Topic to Note complete

--- Enotes/Controller/SingleNoteViewController.swift
+++ Enotes/Controller/SingleNoteViewController.swift
@@ -7,7 +7,39 @@
 //
 
 import UIKit
+import CoreData
 
 class SingleNoteViewController: UIViewController {
-
+    
+    @IBOutlet weak var textbox: UITextView!
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var selectedTopic : Topic? {
+        didSet {
+            loadData()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Model manipulation methods
+    func saveData() {
+        do {
+            try context.save()
+        } catch {
+            print("failed to save, \(error)")
+        }
+    }
+    
+    func loadData() {
+        let request : NSFetchRequest<Note> = Note.fetchRequest()
+        do {
+            var temp = try context.fetch(request)
+            print(temp)
+        } catch {
+            print("failed to fetch, \(error)")
+        }
+    }
 }
--- Enotes/Controller/TopicsViewController.swift
+++ Enotes/Controller/TopicsViewController.swift
@@ -7,85 +7,90 @@
 //
 
 import UIKit
+import CoreData
 
 class TopicsViewController: UITableViewController {
     
     var topics = [Topic]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.loadData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return topics.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NoteCustomCell", for: indexPath)
+        cell.textLabel?.text = topics[indexPath.row].title
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    // MARK: - Table view delegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToNote", sender: self)
+        
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add Topic", message: "Add a topic", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            let newTopic = Topic(context: self.context)
+            newTopic.title = textField.text!
+            self.topics.append(newTopic)
+            self.saveData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Add Topic"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    // MARK: - Data manipulation method
+    
+    func saveData() {
+        do {
+            try context.save()
+        } catch {
+            print("failed to save, \(error)")
+        }
+        tableView.reloadData()
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
+    
+    func loadData() {
+        let request : NSFetchRequest<Topic> = Topic.fetchRequest()
+        do {
+            topics = try context.fetch(request)
+        } catch {
+            print("failed to fetch, \(error)")
+        }
     }
-    */
-
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        var newView = segue.destination as! SingleNoteViewController
+        if let indexPath = tableView.indexPathForSelectedRow {
+            newView.selectedTopic = topics[indexPath.row]
+        }
     }
-    */
 
 }
